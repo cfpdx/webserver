a325ec462e1a371ab46d58430136826bb9531d97
Trevor Johnson <tjohnson1293@gmail.com>
2023-11-07 13:20:27 -0800

    Updated packer config to reflect FE/BE split

--- packer/scripts/appbootstrap.sh
+++ packer/scripts/appbootstrap.sh
@@ -5,18 +5,23 @@
 ## APP
 cd ~
 
-# Copy git SSH Key config from SSM params 
-aws ssm get-parameter --name webGitSSH --with-decryption --output text --query Parameter.Value > .ssh/gitkey
-chmod 600 .ssh/gitkey
+# Copy git SSH Keys config from SSM params 
+aws ssm get-parameter --name webServerGit --with-decryption --output text --query Parameter.Value > .ssh/cfpdxserver
+aws ssm get-parameter --name webServerGit --with-decryption --output text --query Parameter.Value > .ssh/cfpdxclient
+chmod 600 .ssh/cfpdxserver
+chmod 600 .ssh/cfpdxclient
 sudo cp /tmp/sshconfig .ssh/config
 
 # Configure Agent
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/gitkey
+ssh-add ~/.ssh/cfpdxserver
+ssh-add ~/.ssh/cfpdxclient
 
-# Clone application repo
-GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new" git clone git@github.com:sherretten/cfpdx-web.git
-cd cfpdx-web
+# Clone application repos
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new" git clone git@github.com-webserver:cfpdx/webserver.git
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new" git clone git@github.com-client:cfpdx/client.git
+
+cd webserver
 
 # Install essential application packages
 sudo npm ci
