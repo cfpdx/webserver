fdea1b1da3f9867cd9a72ae9d042e1c4e39c0791
Trevor Johnson <tjohnson1293@gmail.com>
2023-11-20 12:51:29 -0800

    Fixed the way the server handles 404's

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 // Handle all other routes by serving the React app's "index.html"
-router.get('/', (req, res) => {
+router.get('/*', (req, res) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
